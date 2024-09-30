14e08a3bf7cc09d0b7f83c6d282e4aa8e1e7d51d
Thomas Blake <tb69613@email.vccs.edu>
2024-09-30 17:54:20 -0400

    Changes

---
+++ addressBookType.cpp
@@ -0,0 +1,1 @@
+#include "addressBookType.h"
---
+++ addressBookType.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <iostream>
+#include <iomanip>
+#include "extPersonType.h"
+#include "personType.h"
+#include "AddressType.h"
+#include "DateType.h"
+#include "AddressBookData.txt"
+using namespace std;
+
+class addressBookType
+{
+private:
+	string addressList[500];
+	int length;
+	int maxLength;
+public:
+	void initEntry(string )
+};
+
